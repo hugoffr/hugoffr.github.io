0898a094e09c2e5d7f230f55e5bcb54e9b8a1503
Hugo Resende <resendehugo@users.noreply.github.com>
2019-12-05 10:14:20 +0000

    Added  a script to solve the sum set sum problem using the brute force method

---
+++ JS/sumset_sum_problem_bruteforce.js
@@ -0,0 +1,99 @@
+function getsumset (id) {
+    var str = document.getElementById(id).value, i;
+    str = str.replace(",",".");
+    str = str.replace(/[^0-9. ]+/g,"");
+    str = str.split(/\s+\s*/);
+    var arr = [];
+    for ( i = 0; i < str.length; i += 1) {
+        if (!isNaN(parseFloat(str[i]))) {
+            console.log(str[i])
+        arr.push(parseFloat(str[i]))
+        }
+    }
+    return arr;
+}
+
+function getexpectedvalue (id) {
+    var str = document.getElementById(id).value;
+    str = str.replace(",",".");
+    var val = parseFloat(str);
+    return val;
+}
+
+function gettol (id) {
+    var str = document.getElementById(id).value;
+    str = str.replace(",",".");
+    var val = parseFloat(str);
+    if (isNaN(val)) {
+        return 0;
+    }
+    return val;
+}
+
+function solver(sum_set, expected_value) {
+    "use strict";
+    var i, j,
+        
+        len_n = sum_set.length,
+        iter = 0,
+        arr_sums = [],
+        arr_sol = [],
+        mask = 0;
+    
+    for (i = 0; i < (Math.pow(2, len_n) - 1); i += 1) {
+        
+        var solution = [];
+        //Apply binary mask to solution
+        iter += 1;
+        for (j = 0; j < len_n; j += 1) {
+            mask = iter >> j & 1;
+            solution.push(mask * sum_set[j]);
+        }
+    
+        
+    var sum_ = solution.reduce(function(acc, val) { return acc + val; }, 0); //Sum of all elements
+    
+    arr_sol.push([sum_,[solution]]);
+        
+    }
+    return arr_sol;
+}
+
+function find_solution_range (arr, value, tol) {
+    var new_arr = [];
+    if (isNaN(value)) {
+        return arr;
+    }
+    for (var i = 0; i < arr.length; i += 1) {
+        if (arr[i][0] <= value + tol && arr[i][0] >= value - tol) {
+            new_arr.push(arr[i])
+        }
+    }
+    return new_arr;
+}
+
+function printer (arr) {
+    var i,
+        str = "";
+    
+    for ( i = 0; i < arr.length ; i += 1 ) {
+        str += arr[i][0].toString() + "<br>" + "[" + arr[i][1].toString() + "]<br>";
+    }
+    
+    return str;
+}
+
+function main() {
+    "use strict";
+    var id_sumset = "sumset",
+        id_expectedvalue = "expectedvalue",
+        id_tol = "tolerance",
+        sum_set = getsumset(id_sumset),
+        expectedvalue = getexpectedvalue(id_expectedvalue),
+        tol = gettol(id_tol);
+    var result = find_solution_range(solver(sum_set), expectedvalue, tol);
+    document.getElementById("demo").innerHTML = printer(result);
+
+}
+
+main();
